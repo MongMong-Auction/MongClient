53938c6a27f8dcef67c28b3931a2602da354cd96
최우영 <dndud159@hconnect.co.kr>
2022-08-21 17:53:44 +0900

    MMA-18 feat: Prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    useTabs: false,
+    arrowParens: 'avoid',
+};
